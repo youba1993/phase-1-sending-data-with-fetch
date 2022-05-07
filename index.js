d591643b189c3c518ac67b86b61af89d7fc1f147
youbaidjis <youba.etud@gmail.com>
2022-05-07 01:10:15 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(userName,userEmail){
+     return   fetch('http://localhost:3000/users',{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify({
+                name: `${userName}`,
+                email: `${userEmail}`,
+            }),
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            const idApp = document.querySelector('body');
+            idApp.innerHTML = `
+            <p>${data.id}</p>
+            `
+        })
+        .catch((error) => {
+            alert("We have an error");
+            const errApp = document.querySelector('body');
+            errApp.innerHTML = `
+            <p>${error.message}</p>
+            `
+        })
+}
